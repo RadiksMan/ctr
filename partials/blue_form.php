64e1e11a1093551ba9697b82fab2a362126e0bf8
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-15 12:10:03 +0300

    no message

---
+++ partials/blue_form.php
@@ -0,0 +1,24 @@
+<div class="blue-form-wrapper">
+	<div class="wrapper">
+		<div class="blue-form shaped">
+			<div class="blue-from-imageblock">
+				<div class="shapes"></div>
+				<div class="illustration"></div>
+			</div>
+			<div class="form-blue">
+				<div class="form-logo">
+					<img src="images/form-logo.png" alt=""/>
+					<p>Свяжитесь с нами <span>прямо сейчас</span></p>
+				</div>
+				<form action="" method="POST">
+					<input type="text" name="name" placeholder="имя"/>
+					<input type="text" name="mail" placeholder="e-mail"/>
+					<input type="text" name="message" placeholder="ваше сообщение"/>
+					<button type="submit">
+						<span>Отправить</span>
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
