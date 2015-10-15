e93c4f2ebf4c92fc21fbd4d529e21c8fb6ea1a3c
Radiks <radion91@gmail.com>
2015-10-15 11:26:54 +0200

    basic php

---
+++ index.php
@@ -0,0 +1,14 @@
+<main class="main mbox">
+	<section>
+		<?partial('red_section');?>
+	</section>
+	<section>
+		<?partial('promoblock');?>
+	</section>
+	<section>
+		<?partial('we_create');?>
+	</section>
+	<section>
+		<?partial('blue_form');?>
+	</section>
+</main>
