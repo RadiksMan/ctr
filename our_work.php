23f66e06e33625d448ec153977879b72d209bad0
Roma Kapustian <wff.kapustianroma@gmail.com>
2015-10-19 15:39:18 +0300

    no message

---
+++ our_work.php
@@ -0,0 +1,209 @@
+<div class="our-works-wrap">
+	<div class="our-works">
+		<div class="title-top">
+			<span>&#032;&#047;&#032;Наши работы</span>
+		</div>
+		<div class="our-works-list cfix">
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork1.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork2.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork3.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork4.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+						<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork1.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork2.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork3.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork4.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+						<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork1.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork2.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork3.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork4.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+						<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork1.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork2.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork3.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork4.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork1.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork2.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork3.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+			<div class="our-work-item">
+				<div class="our-work-image">
+					<img src="images/ourwork4.jpg" alt=""/>
+				</div>
+				<div class="our-work-desc">
+					<h2>Adindex</h2>
+					<p>Лендинг сервисов AdIndex</p>
+					<a href="#">Подробнее <span>&raquo;</span></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
