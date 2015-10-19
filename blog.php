74d7c90698dbce08adbf96bf446b92c1f37166b4
Radiks <radion91@gmail.com>
2015-10-19 16:13:39 +0200

    blog 60%

---
+++ blog.php
@@ -0,0 +1,170 @@
+<main class="main">
+    <section>
+        <div class="blog_page">
+
+            <div class="title_top">
+                <span>/&nbsp;&nbsp;НАШ БЛОГ</span>
+            </div>
+            <div class="blog_page_wrap ">
+                <div class="blog_page_table">
+                    <div class="blog_item animate-section">
+                        <div class="blog_item_cell item_left cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_top_line blue cfix">
+                                    <div class="blog_item_container cfix">
+                                        <div class="top_item_desc">
+                                            <div class="top_item_title">УТОЛЯЕМ ТВОРЧЕСКИЙ ГОЛОД</div>
+                                            <div class="top_item_subtitle">самых разных заказчиков</div>
+                                        </div>
+                                        <div class="top_item_time">
+                                            <span>16.10.2015</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="blog_item_middle cfix">
+                                    <div class="blog_item_container">
+                                        <div class="middle_text">
+                                            <div class="middle_text_wrap">
+                                                <p>Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков</p>
+                                            </div>
+                                        </div>
+                                        <div class="middle_social">
+                                            <div class="middle_social_wrap">
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_1.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_2.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_3.png" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog_item_cell item_right cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_img">
+                                    <img src="images/blog_item_img.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog_item animate-section">
+                        <div class="blog_item_cell item_left cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_top_line green cfix">
+                                    <div class="blog_item_container cfix">
+                                        <div class="top_item_desc">
+                                            <div class="top_item_title">УТОЛЯЕМ ТВОРЧЕСКИЙ ГОЛОД</div>
+                                            <div class="top_item_subtitle">самых разных заказчиков</div>
+                                        </div>
+                                        <div class="top_item_time">
+                                            <span>16.10.2015</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="blog_item_middle cfix">
+                                    <div class="blog_item_container">
+                                        <div class="middle_text">
+                                            <div class="middle_text_wrap">
+                                                <p>Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков</p>
+                                            </div>
+                                        </div>
+                                        <div class="middle_social">
+                                            <div class="middle_social_wrap">
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_1.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_2.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_3.png" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog_item_cell item_right cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_img">
+                                    <img src="images/blog_item_img.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog_item animate-section">
+                        <div class="blog_item_cell item_left cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_top_line orange cfix">
+                                    <div class="blog_item_container cfix">
+                                        <div class="top_item_desc">
+                                            <div class="top_item_title">УТОЛЯЕМ ТВОРЧЕСКИЙ ГОЛОД</div>
+                                            <div class="top_item_subtitle">самых разных заказчиков</div>
+                                        </div>
+                                        <div class="top_item_time">
+                                            <span>16.10.2015</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="blog_item_middle cfix">
+                                    <div class="blog_item_container">
+                                        <div class="middle_text">
+                                            <div class="middle_text_wrap">
+                                                <p>Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков Утоляем творческий голод самых разных заказчиков</p>
+                                            </div>
+                                        </div>
+                                        <div class="middle_social">
+                                            <div class="middle_social_wrap">
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_1.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_2.png" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="middle_social_item">
+                                                    <a href="#">
+                                                        <img src="images/blog_social_3.png" alt="">
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog_item_cell item_right cfix animate-it">
+                            <div class="blog_item_cell_wrap">
+                                <div class="blog_item_img">
+                                    <img src="images/blog_item_img.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
