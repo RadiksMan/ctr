de4e74be74b588ac7b405ce31cebb22998a4cadc
Dmitry <dmitry1940@mail.ru>
2015-10-19 14:25:03 +0300

    recomendations added markup

---
+++ recomendations.php
@@ -0,0 +1,341 @@
+<main class="main">
+    <section>
+        <div class="wrapper recomendations-wrapper">
+            <span>/&nbsp;&nbsp;НАШ БЛОГ</span>
+            <div class="recomendations-main">
+                <div class="recomendation-slider">
+                    <div class="recomendation-item">
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="recomendation-item">
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="recomendation-item">
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto1.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto2.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="recomendation-comment">
+                            <div class="recomendation-user">
+                                <img src="images/user-foto3.png" alt="" />
+                            </div>
+                            <div class="recomendation-title">
+                                <div class="recomendation-name"></div>
+                                <div class="recomendation-post"></div>
+                            </div>
+                            <div class="recomendation-content">
+                                <p></p>
+                            </div>
+                            <div class="recomendation-link">
+                                <a href="#">
+                                    <span class="recomendation-link-text"></span>
+                                    <span class="recomendation-link-icon"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
