4513507e6eb8c9729f5ce45b1fb0c785216f6ca7
trolizmaslom <trolizmaslom@gmail.com>
2015-10-20 10:34:48 +0300

    team fix

--- partials/team.php
+++ partials/team.php
@@ -1,49 +1,50 @@
 
 <div class="wrapper container-team">
     <div class="wrapper-main team-align">
-        <div class="man-mbox">
-            <div class="box-wrapper-team">
-                <div class="man-arrow"></div>
-                <div class="man-mame"><span>Vlad</span></div>
-                <div class="man-prof"><span>[designer]</span></div>
-                <div class="man-foto"><img src="images/portrait_vlad.png"></div>
+        <canvas id="canvas" width="1920" height="817" style="background-color:#FFFFFF"></canvas>
+        <div class="items-wrapper">
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Vlad</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_vlad.png"></div>
+                </div>
             </div>
-        </div>
-        <div class="man-mbox">
-            <div class="box-wrapper-team">
-                <div class="man-arrow"></div>
-                <div class="man-mame"><span>Sergey</span></div>
-                <div class="man-prof"><span>[designer]</span></div>
-                <div class="man-foto"><img src="images/portrait_serg.png"></div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Sergey</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_serg.png"></div>
+                </div>
             </div>
-        </div>
-        <div class="man-mbox">
-            <div class="box-wrapper-team">
-                <div class="man-arrow"></div>
-                <div class="man-mame"><span>Alexander</span></div>
-                <div class="man-prof"><span>[art-director]</span></div>
-                <div class="man-foto"><img src="images/portrait_midd.png"></div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Alexander</span></div>
+                    <div class="man-prof"><span>[art-director]</span></div>
+                    <div class="man-foto"><img src="images/portrait_midd.png"></div>
+                </div>
             </div>
-        </div>
-        <div class="man-mbox">
-            <div class="box-wrapper-team">
-                <div class="man-arrow"></div>
-                <div class="man-mame"><span>Nikolay</span></div>
-                <div class="man-prof"><span>[designer]</span></div>
-                <div class="man-foto"><img src="images/portrait_nick.png"></div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Nikolay</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_nick.png"></div>
+                </div>
             </div>
-        </div>
-        <div class="man-mbox">
-            <div class="box-wrapper-team">
-                <div class="man-arrow"></div>
-                <div class="man-mame"><span>Alexey</span></div>
-                <div class="man-prof"><span>[designer]</span></div>
-                <div class="man-foto"><img src="images/portrait_alex.png"></div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Alexey</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_alex.png"></div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div>
-    <canvas id="canvas" width="1920" height="817" style="background-color:#FFFFFF"></canvas>
-</div>
+
