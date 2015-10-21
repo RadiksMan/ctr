f9396f40094e2aac6208cfa2c7afba47c86c1782
Dmitry <dmitry1940@mail.ru>
2015-10-21 14:16:39 +0300

    fix for rectabges

--- footer.php
+++ footer.php
@@ -1,118 +1,119 @@
-	<div class="footer_placeholder"></div>
-	<footer class="footer">
-        <?php if($act == "index" || $act == "none"){?>
-            <!-- only on index page-->
-            <script src="http://vjs.zencdn.net/5.0.0/video.js"></script>
-        <?}?>
-		<div class="footer-container">
-            <div class="foot-content cfix">
-                <div class="foot-mbox">
-                    <div class="foot-mbox-align">
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>о нас</span>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>работы</span>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>рекомендации</span>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>команда</span>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>блог</span>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#">
-                                <span>связаться с нами</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="foot-mbox">
-                    <div class="foot-mbox-align">
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-sk" src="images/icon-skype.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>a.lymarchuk</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-ml" src="images/icon-mail.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>a.lymarchuk@gmail.com</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-ph" src="images/icon-phone.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>+38 (093) 999 99 99</span>
-                                </div>
-                            </a>
+    	<div class="footer_placeholder"></div>
+    	<footer class="footer">
+            <?php if($act == "index" || $act == "none"){?>
+                <!-- only on index page-->
+                <script src="http://vjs.zencdn.net/5.0.0/video.js"></script>
+            <?}?>
+    		<div class="footer-container">
+                <div class="foot-content cfix">
+                    <div class="foot-mbox">
+                        <div class="foot-mbox-align">
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>о нас</span>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>работы</span>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>рекомендации</span>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>команда</span>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>блог</span>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#">
+                                    <span>связаться с нами</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="foot-mbox">
-                    <div class="foot-mbox-align">
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-vk" src="images/icon-vk.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>vkontakte</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-fb" src="images/icon-fb.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>facebook</span>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="foot-menu-element">
-                            <a href="#" class="foot-icon-box">
-                                <div class="icon-box">
-                                    <img class="ic-gg" src="images/icon-gg.svg">
-                                </div>
-                                <div class="icon-text">
-                                    <span>google plus</span>
-                                </div>
-                            </a>
+                    <div class="foot-mbox">
+                        <div class="foot-mbox-align">
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-sk" src="images/icon-skype.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>a.lymarchuk</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-ml" src="images/icon-mail.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>a.lymarchuk@gmail.com</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-ph" src="images/icon-phone.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>+38 (093) 999 99 99</span>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="foot-copyright">
-                <span>© ctr.design studio 2015</span>
+                    <div class="foot-mbox">
+                        <div class="foot-mbox-align">
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-vk" src="images/icon-vk.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>vkontakte</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-fb" src="images/icon-fb.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>facebook</span>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="foot-menu-element">
+                                <a href="#" class="foot-icon-box">
+                                    <div class="icon-box">
+                                        <img class="ic-gg" src="images/icon-gg.svg">
+                                    </div>
+                                    <div class="icon-text">
+                                        <span>google plus</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="foot-copyright">
+                    <span>© ctr.design studio 2015</span>
+                    </div>
                 </div>
             </div>
-        </div>
-	</footer>
+    	</footer>
+    </div>
 </body>
 </html>
