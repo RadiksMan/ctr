7a028acc6c28177fdd7b0116c1c241520451717b
Dmitry <dmitry1940@mail.ru>
2015-10-30 14:33:32 +0200

    added sendwich to main header

--- partials/mainHeader.php
+++ partials/mainHeader.php
@@ -31,13 +31,46 @@
             </div>
         </a>
         <div class="block-nav">
+            <span class="sendwich-icon">
+              <span></span>
+            </span>
             <nav>
                 <ul>
-                   <li><a href="">о нас </a></li>
-                   <li><a href="">работы</a></li>
-                   <li><a href="">Рекомендации</a></li>
-                   <li><a href="">команда</a></li>
-                   <li><a href="">блог</a></li>
+                    <li>
+                        <a href="#">
+                            <span class="nav-table">о нас </span>
+                            <span class="nav-device">о нАС</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="nav-table">работы</span>
+                            <span class="nav-device">НАШИ РАБОТЫ</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="nav-table">Рекомендации</span>
+                            <span class="nav-device">НАШИ РЕКОМЕНДАЦИИ</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="nav-table">команда</span>
+                            <span class="nav-device">НАША КОМАНДА</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="nav-table">блог</span>
+                            <span class="nav-device">НАШ БЛОГ</span>
+                        </a>
+                    </li>
+                    <li class="device-nav-item">
+                        <a href="#">
+                            <span class="nav-device">СВЯЗАТЬСЯ С НАМИ</span>
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </div>
