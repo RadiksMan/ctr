7266db55eb30f6e89b470f27231642606e05b4b3
trolizmaslom <trolizmaslom@gmail.com>
2015-11-02 09:55:46 +0200

    fix

--- partials/team.php
+++ partials/team.php
@@ -4,7 +4,7 @@
         <div class="title-top animate-section"><span class="animate-it">/ наша команда</span></div>
 
 
-        <div class="items-wrapper">
+        <div class="items-wrapper on-canv">
             <div class="man-mbox">
                 <div class="box-wrapper-team">
                     <div class="man-arrow"></div>
@@ -54,6 +54,49 @@
         <div class="pseudo-canv">
             <div class="pseudo-wrapp">
                 <div class="pseudo-alifn"><img src="images/team.jpg" alt=""></div>
+            </div>
+        </div>
+
+        <div class="blue-items items-wrapper">
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Vlad</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_vlad.png"></div>
+                </div>
+            </div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Sergey</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_serg.png"></div>
+                </div>
+            </div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Alexander</span></div>
+                    <div class="man-prof"><span>[art-director]</span></div>
+                    <div class="man-foto"><img src="images/portrait_midd.png"></div>
+                </div>
+            </div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Nikolay</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_nick.png"></div>
+                </div>
+            </div>
+            <div class="man-mbox">
+                <div class="box-wrapper-team">
+                    <div class="man-arrow"></div>
+                    <div class="man-mame"><span>Alexey</span></div>
+                    <div class="man-prof"><span>[designer]</span></div>
+                    <div class="man-foto"><img src="images/portrait_alex.png"></div>
+                </div>
             </div>
         </div>
     </div>
